191c62f749d387ef102246145fc0fd2278850020
CateNjeri <njerikaris2@gmail.com>
2016-11-22 16:31:55 +0300

    first calculator operation

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+
+function add(number1, number2) {
+        var num = number1 + number2;
+      document.write(num);
+      };
+add(number1, number2);
